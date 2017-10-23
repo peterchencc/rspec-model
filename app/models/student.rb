d6b2592d2ff6f13758c31b9b966f459fbe77e015
peterchencc <peterchencc@gmail.com>
2017-10-23 15:42:08 +0800

    add validate and rspec

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: :true
+  validates :birth_date, presence: true
+
 end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Student, type: :model do
+
+  it { expect(subject).to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:birth_date) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+
+end
