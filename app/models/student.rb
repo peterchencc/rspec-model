4fe1f99796d2164fa83f4b2876af8daaaf573092
peterchencc <peterchencc@gmail.com>
2017-10-23 16:48:34 +0800

    add association for bootcamp

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ApplicationRecord
 
+  belongs_to :bootcamp
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: :true
   validates :birth_date, presence: true
--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :student do
+    bootcamp
     name 'First Name'
     birth_date Date.current
     sequence :email { |n| "email#{n}@example.com" }
